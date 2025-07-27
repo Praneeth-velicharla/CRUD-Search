6544cb63d5ba9f674f11cffa59d16d93603022d9
Praneeth-velicharla <praneethvelicharla2003@gmail.com>
2025-07-27 23:11:17 +0530

    commit

---
+++ crudapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=277)
+
+    def __str__(self):
+        return str(self.id)
+
